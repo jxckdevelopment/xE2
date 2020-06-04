ba163a701dd51a020ebc60aec5c5865da6ad316b
Jxck <52900440+jxckdevelopment@users.noreply.github.com>
2020-06-03 23:56:53 -0700

    v1 - Release

--- NPC Action/NPC Action [v1].lua
+++ NPC Action/NPC Action [v1].lua
@@ -1,1 +1,38 @@
+@name xE2 | NPC Action [v1] | www.jxckdev.co
+ 
+#[
 
+This script was made by JxckDev @ www.jxckdev.co 
+
+]#
+ 
+##################################################
+################## Config ########################
+##################################################
+ 
+Message = "Visit JxckDev.co"
+ 
+##################################################
+################# End Config #####################
+##################################################
+######## DO NOT EDIT BELOW THIS POINT! ###########
+##################################################
+ 
+interval(10)
+ 
+if(first()) {
+    printColor(vec(200,200,200),"[",vec(0,255,255),"xE2 NPC Action",vec(200,200,200),"]",vec(255,255,255)," Finished loading. \n Visit jxckdev.co for free E2 scripts.") # Because this code is free please do not remove this
+}
+ 
+for(I=1,players():count()) {
+    if(players()[I,entity]:aimEntity() == owner() & players()[I,entity]:keyUse() == 1) {
+        Ply = players()[I,entity]:name():explode(" ")
+        if(Ply[2,string] != "") {
+            concmd("say /pm "+Ply[2,string]+" "+Message)
+        }
+        if(Ply[1,string] != "") {
+            concmd("say /pm "+Ply[1,string]+" "+Message)
+        }
+       
+    }
+}
